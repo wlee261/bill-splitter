ede0b3f3b548dd0c50d9bf5a99178339d1292779
wlee261 <wlee@umail.ucsb.edu>
2024-06-22 18:10:49 -0700

    feat: tailwind and prettier setup

--- bill-splitter-app/astro.config.mjs
+++ bill-splitter-app/astro.config.mjs
@@ -1,14 +1,14 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-import htmx from 'astro-htmx';
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import htmx from "astro-htmx";
 
-import node from '@astrojs/node';
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), htmx()],
-  output: 'server',
+  output: "server",
   adapter: node({
-    mode: 'standalone',
+    mode: "standalone",
   }),
 });
